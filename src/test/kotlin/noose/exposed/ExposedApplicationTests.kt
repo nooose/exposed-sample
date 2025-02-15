7d334bd0a20d16d6169240c44f2f04c97a93d744
nooose <hd15807@gmail.com>
2025-02-15 19:15:31 +0900

    feat: setup

---
+++ src/test/kotlin/noose/exposed/ExposedApplicationTests.kt
@@ -0,0 +1,13 @@
+package noose.exposed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExposedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
